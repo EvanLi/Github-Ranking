037e23be5ab7de4d0130f31cf5396f75fc1b436c
EvanLi <EvanLi@users.noreply.github.com>
2019-04-24 13:56:34 +0800

    Edit auto_run.sh.

--- auto_run.sh
+++ auto_run.sh
@@ -2,6 +2,7 @@
 # crontab auto run this file
 cd /home/li/code/github/Github-Ranking
 git pull
+source /home/li/tf36/bin/activate
 python save_most_stars_forks.py
 git add .
 today=`date +"%Y-%m-%d"`
