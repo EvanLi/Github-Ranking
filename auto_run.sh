74ddd872ea2b6ee253aafca878e29f86976c7d8c
EvanLi <EvanLi@users.noreply.github.com>
2019-06-06 23:23:01 +0800

    Edit auto_run.sh.

--- auto_run.sh
+++ auto_run.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 # crontab auto run this file
+# crontab command:
+# 0 12 * * * nohup sh /home/li/code/github/Github-Ranking/auto_run.sh >> /home/li/tmp/Github-Ranking.log 2>&1 &
+
+# show run time
 echo -e "\n----------Run Time:----------"
 date
 cd /home/li/code/github/Github-Ranking
