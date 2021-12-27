952a81b1a7e76d9a61d540292d3514f61dba1635
Kfir Ezekiel <kfir.ye@gmail.com>
2021-12-27 23:13:42 +0200

    updated for crontab

--- auto_run.sh
+++ auto_run.sh
@@ -1,5 +1,11 @@
+#!/bin/sh
+# crontab auto run this file
+# crontab command:
+# 0 12 * * * nohup sh /home/kfiry/Github-Ranking/auto_run.sh >> /home/kfirt/tmp/Github-Ranking.log 2>&1 &
+
 echo -e "\n----------Run Time:----------"
 date
+cd /home/kfiry/Github-Ranking
 git pull
 python source/process.py
 git add .
