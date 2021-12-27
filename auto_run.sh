74c3d76e6956d5c534be665a237531b7f7890b2c
Kfir Ezekiel <kfir.ye@gmail.com>
2021-12-27 22:32:09 +0200

    Update auto_run.sh

--- auto_run.sh
+++ auto_run.sh
@@ -1,14 +1,9 @@
 #!/bin/sh
-# crontab auto run this file
-# crontab command:
-# 0 12 * * * nohup sh /home/li/code/github/Github-Ranking/auto_run.sh >> /home/li/tmp/Github-Ranking.log 2>&1 &
 
 # show run time
 echo -e "\n----------Run Time:----------"
 date
-cd /home/li/code/github/Github-Ranking
 git pull
-source /home/li/tf36/bin/activate
 python source/process.py
 git add .
 today=`date +"%Y-%m-%d"`
